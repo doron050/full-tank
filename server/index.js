87993b2f7a7482b81e493176715b673adbee15d5
dror <dror@sears.co.il>
2018-09-12 20:20:16 +0300

    Update server port

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 // returns the 'id' argument that was given
 app.get('/test', (req, res) => {
     res.send('id: ' + req.query.id);
 });
 
-app.listen(3000, () => console.log('full-tank app listening on port 3000!'));
+app.listen(port, () => console.log('full-tank app listening on port 3000!'));
