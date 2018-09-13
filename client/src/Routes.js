b1758d64d824898dd73f879fc7d599ee816a9b8f
Doron <doron@sears.co.il>
2018-09-13 17:25:29 +0300

    remove location page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,7 +3,6 @@ import {HashRouter as Router, Route} from "react-router-dom";
 import App from "./App";
 import HomePageContainer from "./pages/HomePageContainer";
 import AboutPage from "./pages/AboutPage";
-import LocationPage from './pages/LocationPage';
 import CreateOrderPageContainer from './driver/create-order-page/CreateOrderPage';
 
 export default function Routes() {
@@ -12,7 +11,6 @@ export default function Routes() {
             <App>
                 <Route exact path="/" component={HomePageContainer}/>
                 <Route path="/about" component={AboutPage}/>
-                <Route path={"/location"} component={LocationPage}/>
 
                 {/*Driver*/}
                 <Route path={"/new-order"} component={CreateOrderPageContainer}/>
