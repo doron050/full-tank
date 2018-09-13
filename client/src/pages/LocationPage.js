f77765dc981bb353694ee089e80580536e1f76a3
Doron <doron@sears.co.il>
2018-09-13 14:57:12 +0300

    add address function

--- client/src/pages/LocationPage.js
+++ client/src/pages/LocationPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {geolocated} from 'react-geolocated';
 import GoogleMapReact from 'google-map-react';
+import Geocode from 'react-geocode';
+
 //
 // export default function AboutPage() {
 //     return (
@@ -21,7 +23,40 @@ class MarkMap extends Component {
 }
 
 class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            address: ""
+        };
+    }
+
+
+    getAddress() {
+        if(!this.props.coords)
+            return;
+
+        const lat = this.props.coords.latitude;
+        const lng = this.props.coords.longitude;
+        Geocode.setApiKey("AIzaSyCyRr2zIViTQjYzgOVQwCNEasVXYDsVHO8");
+        Geocode.fromLatLng(lat, lng).then(
+            response => {
+                const address = response.results[0].formatted_address;
+                this.setState({
+                    address: address
+                });
+            },
+            error => {
+                this.setState({
+                    address: error.toString()
+                });
+            }
+        );
+    }
+
+
     render() {
+        this.getAddress.bind(this)();
+
         return !this.props.isGeolocationAvailable
             ? <div>Your browser does not support Geolocation</div>
             : !this.props.isGeolocationEnabled
@@ -31,16 +66,35 @@ class Demo extends React.Component {
                     <div>
                         <table>
                             <tbody>
-                            <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
-                            <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
-                            <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
-                            <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
-                            <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+                            <tr>
+                                <td>latitude</td>
+                                <td>{this.props.coords.latitude}</td>
+                            </tr>
+                            <tr>
+                                <td>longitude</td>
+                                <td>{this.props.coords.longitude}</td>
+                            </tr>
+                            <tr>
+                                <td>altitude</td>
+                                <td>{this.props.coords.altitude}</td>
+                            </tr>
+                            <tr>
+                                <td>heading</td>
+                                <td>{this.props.coords.heading}</td>
+                            </tr>
+                            <tr>
+                                <td>speed</td>
+                                <td>{this.props.coords.speed}</td>
+                            </tr>
+                            <tr>
+                                <td>address</td>
+                                <td>{this.state.address}</td>
+                            </tr>
                             </tbody>
                         </table>
-                        <div style={{ height: '500px', width: '500px' }}>
+                        <div style={{height: '500px', width: '500px'}}>
                             <GoogleMapReact
-                                bootstrapURLKeys={{ key: "AIzaSyAU2lHdJIFWKST_2VfDWlmCChsaAE31xv0" }}
+                                bootstrapURLKeys={{key: "AIzaSyCyRr2zIViTQjYzgOVQwCNEasVXYDsVHO8"}}
                                 defaultCenter={{lat: this.props.coords.latitude, lng: this.props.coords.longitude}}
                                 defaultZoom={9}
                             >
@@ -54,6 +108,8 @@ class Demo extends React.Component {
                         </div>
                     </div>
                     : <div>Getting the location data&hellip; </div>;
+
+
     }
 }
 
