229c10c13ff3eb6fce3a5e4d9c25815f800b12c5
Doron <doron@sears.co.il>
2018-11-13 18:43:55 +0200

    Updates

---
+++ precache-manifest.9e3d41673846bb0c312856f28372780f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69df1d4e79931db1a864bf0980a07c4f",
+    "url": "/full-tank/static/media/markZ.69df1d4e.jpg"
+  },
+  {
+    "revision": "98930f0bb073c0fa078eecf278c1b858",
+    "url": "/full-tank/static/media/map-marker-icon.98930f0b.png"
+  },
+  {
+    "revision": "6324da1880ae97533369ec8bb813570a",
+    "url": "/full-tank/static/media/black-map-marker.6324da18.jpg"
+  },
+  {
+    "revision": "170bc2013c8917f58637",
+    "url": "/full-tank/static/js/runtime~main.170bc201.js"
+  },
+  {
+    "revision": "b5b37bc49976073b1fa1",
+    "url": "/full-tank/static/js/main.b5b37bc4.chunk.js"
+  },
+  {
+    "revision": "0cedc2de7e934bebabdb",
+    "url": "/full-tank/static/js/1.0cedc2de.chunk.js"
+  },
+  {
+    "revision": "b5b37bc49976073b1fa1",
+    "url": "/full-tank/static/css/main.e07375ba.chunk.css"
+  },
+  {
+    "revision": "9084b59cc798225510251fb01f79f77b",
+    "url": "/full-tank/index.html"
+  }
+];
